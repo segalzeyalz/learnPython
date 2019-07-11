80aef67baf355fe0c12364be17e6eba0ab5b2e26
segalzeyalz <segaleseo@gmail.com>
2019-07-11 12:23:13 +0300

    learn loop

--- index.py
+++ index.py
@@ -1,2 +1,11 @@
+import random
+
 def checkEven(num):
     return num%2==0
+
+for i in range(0,200):
+    x = random.randint(0, 200)
+    if checkEven(x):
+        print(x, " is even")
+    else:
+        print(x, "is odd")
